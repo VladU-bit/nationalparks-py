9e74c9e69d948f6294a555e21005afb89e94b11b
VladU <61273642+VladU-bit@users.noreply.github.com>
2025-03-06 15:17:40 +0000

    Update wsgi.py - Change name

--- wsgi.py
+++ wsgi.py
@@ -47,7 +47,7 @@ api.add_resource(HealthCheck, '/ws/healthz/')
 class Info(Resource):
     description = {
         'id': 'nationalparks-py',
-        'displayName': 'Super-Defunct National Parks',
+        'displayName': 'Very old National Parks',
         'type': 'cluster',
         'center': {'latitude': '47.039304', 'longitude': '14.505178'},
         'zoom': 4
